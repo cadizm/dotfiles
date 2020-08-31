698bd0dee4258d2de98551c25e6dd26525529b97
Michael Cadiz <mike@cadizm.com>
2020-08-31 15:32:05 -0700

    ctrl-e -> accept

--- fzf.bash
+++ fzf.bash
@@ -13,4 +13,4 @@ fi
 source "/usr/local/opt/fzf/shell/key-bindings.bash"
 
 # https://github.com/junegunn/fzf/issues/1414
-export FZF_DEFAULT_OPTS="--bind ctrl-k:kill-line"
+export FZF_DEFAULT_OPTS="--bind ctrl-k:kill-line --bind ctrl-e:accept"
